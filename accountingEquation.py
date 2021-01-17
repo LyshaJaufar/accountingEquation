2036dcece7a59c8a8095e736205816ef2eb33b5f
LyshaJaufar <lysha.ahmd@gmail.com>
2021-01-17 18:11:11 +0500

    update

---
+++ accountingEquation.py
@@ -0,0 +1,49 @@
+from sys import argv, exit
+import csv
+
+
+# Open the csv file
+database = open(argv[1], "r")
+
+# Answer Sheet
+file = open('Accounting Equation - Answer Sheet.csv', 'w')
+if (file == None):
+    exit(1)
+
+# Create DictReader
+reader = csv.DictReader(database)
+
+# Write the fieldnames/column names in row 0
+for i in range(len(reader.fieldnames)):
+    file.write(reader.fieldnames[i] + ',')
+file.write("\n")
+
+for row in reader: 
+    # Variables for names of each column
+    assets = row['Assets'].lower()
+    liabilities = row['Liabilities'].lower()
+    caital = row['Capital'].lower()
+
+    # Assets
+    if assets == "":
+        liabilities = int(input("Liabiilties: "))
+        capital = int(input("Capital: "))
+        assets = capital + liabilities
+
+
+    # Liabilities
+    if liabilities == "":
+        assets = int(input("Assets: "))
+        capital = int(input("Capital: "))
+        liabilities = assets - capital
+
+
+    # Capital
+    if capital == "":
+        assets = int(input("Assets: "))
+        liabilities = int(input("Liabiilties: "))
+        capital = assets - liabilities
+
+    file.write(assets + ",")
+    file.write(liabilities + ",")
+    file.write(capital + "\n") 
